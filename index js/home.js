80c51ed490d050027b6f0ea241be9decd193f9a7
Subham0629 <kandarisubham72@gmail.com>
2022-11-09 22:48:12 +0530

    day 2 work

---
+++ index js/home.js
@@ -0,0 +1,258 @@
+var home_slide=["https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/cover/1920x7704ce388d5a6e74dac8ad9255657da588f.jpg",
+"https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5233808/cover/1920x770e1e200eb2ab84c2bb678ee60e3dcf06b243111bceaad490a94d223a2872f5a7a.jpg",
+"https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206274/cover/1920x770485283887ce04c66a91f3e5dc0e20747.jpg",
+"https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5191161/cover/1920x770bc4eccaecfc244d5abf81008d247152999b6f14eca0d474287fa92038b166e0d.jpg",
+"https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246084/cover/1920x7706643570fad7d4b018eb8ee595ee8f66a.jpg",
+"https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/cover/1920x7701789cd01c98a4b098ff87b399b61530c3e52cc15ff174f4689cc1ebefe5b40ff.jpg",
+"https://akamaividz2.zee5.com/image/upload/w_1013,h_405,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/cover/1920x77061cfc501b93f489b971525bcc03d06c2.jpg"
+];
+
+var home=[
+    //---Trending Near You---//
+{title:"Rashtra Kavach OM",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5191648/portrait/1920x77015755573326f485eb9b2ad856c296abd.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Forensic",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5149852/portrait/1920x770329e7efce50a434cb76540abee106a8c.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Kartikeya 2",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5227565/portrait/1920x7704ce388d5a6e74dac8ad9255657da588f.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Dhaakad",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5168144/portrait/1920x770231e4cce81b8432db798e3051c668bf6c247548b7a24419c9605c50abb4c536b.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Attack Part:1",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5146416/portrait/1920x7708dc79580826949a992f2b011b0e50f28.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"The Kashmir Files",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5133458/portrait/1920x7700b75e1f80c204a0b82dfad6f59fd7f8ce7171601b0534b17aa8968531f3bc8f1.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Tadka",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/portrait/1920x77061cfc501b93f489b971525bcc03d06c2.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"The Python",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5233808/portrait/1920x770e1e200eb2ab84c2bb678ee60e3dcf06b243111bceaad490a94d223a2872f5a7a.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Vikram Vedha",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-101-10z5226524/portrait/1920x770e578d29207064883898fd35bbf51977d.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Raksha Bandhan",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5228525/portrait/1920x7701789cd01c98a4b098ff87b399b61530c3e52cc15ff174f4689cc1ebefe5b40ff.jpg",
+category:"Trending Near You",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+
+{title:"Tripling Season 3",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-6-4z5230436/portrait/1920x770cd5550fd6fe64ed19f73339697501913a1ebe2567d824021883c08f193544046.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Tadka",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5246147/portrait/1920x77061cfc501b93f489b971525bcc03d06c2.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Bimbisara",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5234688/portrait/1920x770b60b8550ec184b5eae16b5b5a3ae4579301be9f1145b4c46a2a8ea62adf5b37a.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Mei Hoom Moosa",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5250010/portrait/1920x77080d48a43c1374c8bb5ea0f857e7b153a.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Antardhaan",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5238929/portrait/1920x770db79ed4e5ed947ca879038eacb99074d.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Captain",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5226617/portrait/1920x770c0fb8727e1894eb3aba1cf889861317c.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Agantuk",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5234581/portrait/1920x770e7575188954d4331afbc52df724a21dd.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Lal Jose",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5226633/portrait/1920x770294f8b508d214358b45a1fa0beae30f5.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Timepass 3",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5215329/portrait/1920x77091eec50955ae4e4c900f95666d758e9b.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Vikrant Rona",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5206541/portrait/1920x7703c0a13556ad947dca8424144c3f19077b3ca91677ded48b8961a9c88d627b156.jpg",
+category:"Latest & Trending",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+
+//---top 10 movies of India---//
+{title:"Love Hostel",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z595840/portrait/1920x7705d9c7a86295d4f268cd4c7deb387d9b8.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Helmet",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z51560/portrait/1920x770087ff5f6d15f44f98b6a27adc7130aac.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Bhavai",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z5214566/portrait/1920x770310aa5c8cd9c431f8f4a81e797a6fd84.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"14 Phere",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z51198/portrait/1920x770a02410f0ba6b487cb2b2309deaff5751.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Dial 100",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z51248/portrait/1920x77014c02c5fa1a34847ae30a271fb74d329.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Rashmi Rocket",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z513117/portrait/1920x770478c174d17894ab79a63d72e6b02a830de9355531fe64e6c9b054018cf7050cf.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Bamfaad",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-145361/portrait/00145361incover.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Kaagaz",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-296370/portrait/1920x770056ba2ae2b59429fbbc45f002f0bcf9c43a75de3954a4001ad153625dbbd5081.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Taish",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-246113/portrait/00246113incover3095172930a0afef527b084c8ca4052fc641893ce7.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"},
+{title:"Bob Biswas",
+image:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco/resources/0-0-1z544916/portrait/1920x770141dcb00909443ec8d04dbc10d8e7f6c944beb98e417497b944a409681c374c0.jpg",
+category:"Top 10 Movies in India",
+time:"2h 7m",
+sub_category:"Action",
+type:"Thriller"}
+]
+
+
+
+
+let x=home_slide.length;
+    let i=0;
+    var z;
+    function display(home_slide){
+        document.querySelector("#home_slide").innerHTML="";
+        let image=document.createElement("img");
+        image.setAttribute("src",home_slide[i]);
+        document.querySelector("#home_slide").append(image)
+        
+    }
+    display(home_slide);
+        z= setInterval(()=>{
+            
+            i++;
+            if(i>x-1){
+                i=0;
+            }
+            display(home_slide)
+        },2000)
+        document.querySelector("#prev").addEventListener("click",()=>{
+            i--;
+            if(i<0){
+                i=x-1;
+            }
+            display(home_slide)
+        })
+        document.querySelector("#nxt").addEventListener("click",()=>{
+            i++;
+            if(i>x-1){
+                i=0;
+            }
+            display(home_slide)
+        })
+
+
+        displayRow(home);
+        function displayRow(home){
+
+            home.forEach(element => {
+            console.log(element.title)
+            let div=document.createElement("div");
+            let image=document.createElement("img");
+            image.setAttribute("src",element.image)
+            div.append(image);
+            document.querySelector(".inbox").append(div);
+                
+            });
+            
+            
+        }
+
+
+
+
+
+
+
+        
+           
